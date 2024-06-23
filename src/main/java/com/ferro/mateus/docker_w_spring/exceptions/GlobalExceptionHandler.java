ddb1b48dba68299002d155f36045effd54086b39
mateeusferro <mateus.03fernandesaraujo@gmail.com>
2024-06-23 20:16:59 -0300

    feat: adding new exceptions

--- src/main/java/com/ferro/mateus/docker_w_spring/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ferro/mateus/docker_w_spring/exceptions/GlobalExceptionHandler.java
@@ -1,19 +1,15 @@
 package com.ferro.mateus.docker_w_spring.exceptions;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpHeaders;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -39,6 +35,24 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({ IllegalArgumentException.class })
     public ResponseEntity<Object> handleIllegalArgumentException(
             IllegalArgumentException ex, WebRequest request) {
+        Map<String, String> body = new HashMap<>();
+        body.put("errors", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ DataIntegrityViolationException.class })
+    public ResponseEntity<Object> handleDataIntegrityViolationException(
+            DataIntegrityViolationException ex, WebRequest request) {
+        Map<String, String> body = new HashMap<>();
+        body.put("errors", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ Exception.class })
+    public ResponseEntity<Object> handleExceptionGeneric(
+            Exception ex, WebRequest request) {
         Map<String, String> body = new HashMap<>();
         body.put("errors", ex.getMessage());
 
